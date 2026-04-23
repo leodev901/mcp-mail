5abd59cb1ff391f2009d75498dede422b367699b
jinsoo-sk-LAPTOP-01 <ljslim27@gmail.com>
2026-04-23 14:30:58 +0900

    feat: 로그 api 호출시 헤더의 authorize 정보로부터 DB 의존성 주입

--- cmn/core/dependencies.py
+++ cmn/core/dependencies.py
@@ -2,6 +2,7 @@ from fastapi import Request, Depends, Header, HTTPException, Query
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from collections.abc import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import DBAPIError, SQLAlchemyError
 
 from cmn.core.database import Database
 from cmn.schemas.user import User
@@ -11,9 +12,33 @@ from loguru import logger
 
 
 
-# db engine을 그대로 받아서 서비스에서 세션 연결
+
 async def get_db(request: Request):
+     """reqeust.state.app에 저장된 db engine을 그대로 반환"""
      return request.app.state.db
+
+
+async def get_db_session_authorize_header(
+    db: Database = Depends(get_db),
+    user: User = Depends(jwt_manager.get_current_user)
+) -> AsyncGenerator[AsyncSession , None]:
+    """Request 헤더의 요청 Authroze(Bearer Token) 으로부터 사용자 정보를 파싱하여 회사 코드로 DB 세션 연결"""
+    schema = user.company_code
+    logger.debug(f"get_db_session_authorize: {schema}")
+
+    async with db.session(schema) as session:
+        try:
+            yield session
+            await session.commit() # 세션에서 DB 작업이 정상적으로 끝나면 commit 으로 마무리 
+
+        except DBAPIError as exc:
+            await session.rollback() # DB 예외가 나면 Rollback
+            logger.error(f"Databse 실행 오류: {exc}")
+            raise exc
+        except Exception:
+            await session.rollback() # 비즈니스 예외도 Rollback
+            raise
+
 
 async def get_db_session_for_compnay(
         request: Request,
