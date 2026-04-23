2401a3c2309ca3a3b6198d08bb1c1272c539a57e
jinsoo-sk-LAPTOP-01 <ljslim27@gmail.com>
2026-04-23 14:28:57 +0900

    chore: 함수 순서 변경

--- cmn/api/endpoint/utils_router.py
+++ cmn/api/endpoint/utils_router.py
@@ -7,10 +7,11 @@ from cmn.utils import jwt_manager
 utils_router = APIRouter(prefix="/utils",tags=["utils"])
 
 
+@utils_router.get("/jwt/decode", response_model=CommonResponse)
+async def jwt_decode(user: User = Depends(jwt_manager.get_current_user)):
+    return CommonResponse.ok(user)
+
 @utils_router.post("/jwt/encode", response_model=CommonResponse)
 async def jwt_encode(user: User):
     return CommonResponse.ok(jwt_manager.encode(user))
 
-@utils_router.get("/jwt/decode", response_model=CommonResponse)
-async def jwt_decode(user: User = Depends(jwt_manager.get_current_user)):
-    return CommonResponse.ok(user)
